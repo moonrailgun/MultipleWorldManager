ea9ade1d39a3f495169c2f42b87db89b7a410c63
moonrailgun <moonrailgun@gmail.com>
2015-09-26 15:04:18 +0800

    数据库封装
    ADD 数据库封装

---
+++ MWMServer/mwmSql.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Chen on 2015-09-26.
+ */
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mwm'
+});
+
+function query(sql, func) {
+    var result = {};
+
+    connection.connect();//打开连接
+    connection.query(sql, function (err, res, fields) {
+        if (err) throw err;
+        func(res);//回调函数
+        //console.log(res);
+    });
+    connection.end();//关闭连接
+}
+
+exports.query = query;
