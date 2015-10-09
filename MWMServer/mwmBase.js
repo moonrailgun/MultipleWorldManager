945f0703634eec577e2cf81150e5dae43805c5f0
moonrailgun <moonrailgun@gmail.com>
2015-10-09 20:52:29 +0800

    创建UUID方法
    ADD 创建UUID

--- MWMServer/mwmBase.js
+++ MWMServer/mwmBase.js
@@ -3,6 +3,7 @@
  */
 
 var mwmSql = require('./mwmSql');
+var randomWord = require('./randomWord');
 
 //应用通用模块
 
@@ -20,33 +21,43 @@ function CheckUUID(uuid, func) {
     });
 }
 
+//给某UID创建UUID并返回创建的值
+function CreateUUID(uid, func) {
+    var uuid = randomWord.Generate(20);
+    var sql = "UPDATE account SET uuid = '" + uuid + "' WHERE uid = '" + uid + "' ";
+    mwmSql.query(sql, function (data) {
+        func(uuid);
+    });
+}
+
 /*
-//无参多任务模块
-//未测试
-var MultiTasks = function(){
-    var taskQueue = [];
-    var taskIndex = 0;
-    var currentIndex = 0;
+ //无参多任务模块
+ //未测试
+ var MultiTasks = function(){
+ var taskQueue = [];
+ var taskIndex = 0;
+ var currentIndex = 0;
 
-    this.Task = function(func){
-        this.taskName = "";
-        this.func = func;//任务函数
-    };
+ this.Task = function(func){
+ this.taskName = "";
+ this.func = func;//任务函数
+ };
 
-    this.push = function(task){
-        taskqueue.push(task);
-    };
+ this.push = function(task){
+ taskqueue.push(task);
+ };
 
-    this.startQueue = function(){
-        if(taskQueue.length > 0)
-        {
-            for(var i = 0;i<taskQueue.length;i++)
-            {
-                var task = taskQueue[i];
-                task.func();
-            }
-        }
-    };
-};*/
+ this.startQueue = function(){
+ if(taskQueue.length > 0)
+ {
+ for(var i = 0;i<taskQueue.length;i++)
+ {
+ var task = taskQueue[i];
+ task.func();
+ }
+ }
+ };
+ };*/
 
 exports.CheckUUID = CheckUUID;
+exports.CreateUUID = CreateUUID;
--- MWMServer/randomWord.js
+++ MWMServer/randomWord.js
@@ -5,20 +5,21 @@
 /*
  ** randomWord 产生任意长度随机字母数字组合
  ** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
- ** xuanfeng 2014-08-28
  */
+//字典
+var dir = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 function randomWord(randomFlag, min, max) {
     var str = "",
-        range = min,
-        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        range = min;
 
     // 随机产生
     if (randomFlag) {
-        range = Math.round(Math.random() * (max - min)) + min;
+        range = Math.round(Math.random() * (max - min)) + min;//范围生成
     }
     for (var i = 0; i < range; i++) {
-        pos = Math.round(Math.random() * (arr.length - 1));
-        str += arr[pos];
+        var pos = Math.round(Math.random() * (dir.length - 1));
+        str += dir[pos];
     }
     return str;
 }
@@ -30,7 +31,6 @@ function Generate(min, max) {
     else {
         return randomWord(true, min, max);
     }
-
 }
 
 exports.Generate = Generate;
